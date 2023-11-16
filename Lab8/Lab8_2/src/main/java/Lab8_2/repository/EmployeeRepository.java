1cace38e8e997f997f65a68bc09788a04c16d2d5
S52100412 <146973012+ThuHaohi@users.noreply.github.com>
2023-11-17 01:27:43 +0700

    Lab8

---
+++ Lab8/Lab8_2/src/main/java/Lab8_2/repository/EmployeeRepository.java
@@ -0,0 +1,7 @@
+package Lab8_2.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import Lab8_2.model.Employee;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
